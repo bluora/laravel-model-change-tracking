2c484a58135eb8bd4355a25d0a8f06553a1c0bcb
Rocco Howard <rocco@bluora.com.au>
2016-03-06 22:52:29 +1100

    First commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Laravel Eloquent Model - user change tracking
+
+Adds events to set a column to the current user for when a model is created, updated or deleted.
+
+## Installation
+
+Require this package in your `composer.json` file:
+
+`"bluora/model-change-tracking": "dev-master"`
+
+Then run `composer update` to download the package to your vendor directory.
+
+## Usage
+
+### Basic
+
+Add a `created_by`, `updated_by` and `deleted_by` columns to your model's database table.
+
+
+```php
+use ModelChangeTracking\ChangeByUserTrait;
+
+class User extends Model
+{
+    use ChangeByUserTrait;
+}
+```
+### Turn off column
+
+You can turn off by returning false.
+```php
+public function getCreatedByColumn()
+{
+    return false;
+}
+
+public function getUpdatedByColumn()
+{
+    return false;
+}
+
+public function getDeletedByColumn()
+{
+    return false;
+}
+```
+
+### Different column name
+
+You can specify the column name in the return value.
+
+```php
+public function getCreatedByColumn()
+{
+    return 'created_by';
+}
+
+public function getUpdatedByColumn()
+{
+    return 'updated_by';
+}
+
+public function getDeletedByColumn()
+{
+    return 'deleted_by';
+}
+```
+
---
+++ src/ModelChangeTracking/ChangeByUserTrait.php
@@ -0,0 +1,55 @@
+<?php
+namespace ModelChangeTracking;
+
+use Auth;
+
+trait ChangeByUserTrait 
+{
+
+    public function getCreatedByColumn()
+    {
+        return 'created_by';
+    }
+
+    public function getUpdatedByColumn()
+    {
+        return 'updated_by';
+    }
+
+    public function getDeletedByColumn()
+    {
+        return 'deleted_by';
+    }
+
+    /**
+     * Boot the events that apply which user is making the last event change
+     *
+     * @return void
+     */
+    public static function bootChangeByUserTrait()
+    {
+        static::creating(function ($model) {
+            if (Auth::check() && $model->getCreatedByColumn()) {
+                $model->{$model->getUpdatedByColumn()} = Auth::user()->id;
+            }
+            if (Auth::check() && $model->getUpdatedByColumn()) {
+                $model->{$model->getUpdatedByColumn()} = Auth::user()->id;
+            }
+            return true;
+        });
+
+        static::updating(function ($model) {
+            if (Auth::check() && $model->getUpdatedByColumn()) {
+                $model->{$model->getUpdatedByColumn()} = Auth::user()->id;
+            }
+            return true;
+        });
+
+        static::deleting(function ($model) {
+            if (Auth::check() && $model->getDeletedByColumn()) {
+                $model->{$model->getDeletedByColumn()} = Auth::user()->id;
+            }
+            return true;
+        });
+    }
+}
