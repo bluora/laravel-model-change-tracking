9089b3512b0e18ae4f5aca20508b7a1110a63c4e
Rocco Howard <rocco@bluora.com.au>
2016-03-06 23:24:53 +1100

    Added example files for the model column change tracking

---
+++ example/LogModel.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LogModel extends Model
+{
+    public $timestamps = false;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'log_model';
+
+    /**v
+     * The attributes that require casting
+     *
+     * @var array
+     */
+    protected $casts = [
+        'model' => 'string',
+        'model_id' => 'integer',
+        'column_name' => 'string',
+        'old_value' => 'string',
+        'new_value' => 'string'
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'model',
+        'model_id',
+        'column_name',
+        'old_value',
+        'new_value',
+        'log_by'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * Get the user who created log
+     */
+    public function logBy()
+    {
+        return $this->hasOne('App\Models\User');
+    }
+}
---
+++ example/create_log_model_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogModelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_model', function ($table) {
+            $table->bigIncrements('id');
+            $table->string('model');
+            $table->integer('model_id');
+            $table->text('old_value');
+            $table->text('new_value');
+            $table->timestamp('log_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->integer('log_by');            
+            $table->foreign('log_by')->references('id')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('log_model', function (Blueprint $table) {
+            $table->dropForeign('log_model_log_by_foreign');
+        });
+        Schema::drop('log_model');
+    }
+}
