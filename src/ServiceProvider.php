09576603256056193dd31ec0af9d1b14281bf7a9
Rocco Howard <rocco@hnh.digital>
2017-04-14 20:55:12 +1000

    Update file locations; Add config publishing, deploy models, and set migrations path

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,16 +7,27 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 class ServiceProvider extends BaseServiceProvider
 {
     /**
-     * Register the service provider.
+     * Perform post-registration booting of services.
      *
      * @return void
      */
     public function boot()
     {
+        // Add the command to artisan.
         if ($this->app->runningInConsole()) {
             $this->commands([
                 ModelSyncCommand::class,
             ]);
         }
+
+        // Publish the default config and models.
+        $this->publishes([
+            __DIR__.'/../config/model_change_tracking.php' => base_path('config/model_change_tracking.php'),
+            __DIR__.'/../models/LogModelChange.php' => base_path('app/models/LogModelChange.php'),
+            __DIR__.'/../models/LogModelStateChange.php' => base_path('app/models/LogModelStateChange.php'),
+        ]);
+
+        // Load migrations.
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
     }
 }
